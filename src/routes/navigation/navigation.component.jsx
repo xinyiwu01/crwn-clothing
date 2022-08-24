1ab1380a51589efc1b4cd27f4dc70fbd3c054487
xinyi <xinyiw2@andrew.cmu.edu>
2022-08-24 00:28:37 -0400

    add styles for nav bar

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,7 @@
 import { Outlet, Link } from 'react-router-dom';
 import { Fragment } from 'react';
-import {ReactComponent as CrwnLogo} from '../../assets/crown.svg'
+import {ReactComponent as CrwnLogo} from '../../assets/crown.svg';
+import './navigation.styles.scss';
 
 const Navigation = () => {
     return (
