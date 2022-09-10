da05b5955a229412bbd6f463689af1d10f5940e1
xinyi <xinyiw2@andrew.cmu.edu>
2022-09-09 21:02:08 -0400

    handle redirect memory with useEffect

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,16 +1,35 @@
-import {signInWithGooglePopup} from '../../utils/firebase/firebase.utils'
+import { 
+    auth, 
+    signInWithGooglePopup, 
+    signInWithGoogleRedirect, 
+    createUserDocumentFromAuth
+} from '../../utils/firebase/firebase.utils'
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+//redirect to a new page/domain, can't track user data
 
 const SignIn = () => {
+    useEffect(async() => {
+        const response = await getRedirectResult(auth);//auth:keep track of all authentication states, memory
+        if(response) {
+            const userDocRef = await createUserDocumentFromAuth(response.user);
+        }
+    }, []);
+
+
     //access database: async
-const logGoogleUser = async () => {
-    const response = await signInWithGooglePopup();
-    console.log(response);
-}
+    const logGoogleUser = async () => {
+        const {user} = await signInWithGooglePopup();
+        const userDocRef = await createUserDocumentFromAuth(user);
+
+    }
+
 
     return (
         <div>
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+            <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button>
         </div>
     );
 }
